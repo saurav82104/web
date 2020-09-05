bcca8b73c0d28c85ab3dfc7d65710b66cb8f1dfa
saurav82104 <69568476+saurav82104@users.noreply.github.com>
2020-09-05 21:05:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ function myFunction() {
   alert("welcome");
 
 }
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
